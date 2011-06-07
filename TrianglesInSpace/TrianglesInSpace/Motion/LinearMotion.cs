895a5b45a7b1b5d11172b960ffa1b7104a5a3970
anthony-martin <martin.anthony.j@gmail.com>
2011-06-07 20:18:57 +0800

    Start transfering Linear motion and testing

---
+++ TrianglesInSpace/TrianglesInSpace/Motion/LinearMotion.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using Mogre;
+
+namespace TrianglesInSpace.Motion
+{
+	public class LinearMotion
+	{
+		private Vector2 m_Velocity;
+
+		public LinearMotion(Vector2 velocity)
+		{
+			m_Velocity = velocity;
+		}
+
+		public Vector2 GetVelocity()
+		{
+			return m_Velocity;
+		}
+
+	}
+}
---
+++ TrianglesInSpace/TrianglesInSpace/Motion/Nunit/LinearMotionTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mogre;
+using NUnit.Framework;
+
+namespace TrianglesInSpace.Motion.Nunit
+{
+	class LinearMotionTests : TestSpecification
+	{
+		
+		[Test]
+		public void Create_LinearMotion()
+		{
+			var vector = new Vector2(1,1);
+			var motion = new LinearMotion(vector);
+		}
+
+		[Test]
+		public void Test_Get_Velocity()
+		{
+			var vector = new Vector2(1, 1);
+			var motion = new LinearMotion(vector);
+
+			Assert.AreEqual(vector , motion.GetVelocity());
+		}
+	}
+}
