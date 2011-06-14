91b3d2b2259fddc05ea7fa2d5ad053cbb2bf2db2
anthony-martin <martin.anthony.j@gmail.com>
2011-06-14 22:24:44 +0800

    Add Angle struct to encapsulate angle logic and distinguish type

---
+++ TrianglesInSpace/TrianglesInSpace/Primitives/Angle.cs
@@ -0,0 +1,103 @@
+﻿using Mogre;
+using Math = System.Math;
+
+namespace TrianglesInSpace.Primitives
+{
+	public struct Angle
+	{
+		private double m_Radians;
+
+		public Angle(double angle)
+		{
+			m_Radians = ReduceAngle(angle);
+		}
+
+		public double Value
+		{
+			get
+			{
+				return m_Radians;
+			}
+		}
+
+		public static double ReduceAngle(double angle)
+		{
+			// make sure the angle between -Pi and Pi not including -Pi
+			while ((angle > Math.PI) || (angle <= (-Math.PI)))
+			{
+				if (angle > Math.PI)
+				{
+					angle = angle - (2 * Math.PI);
+				}
+				else
+				{
+					angle = angle + (2 * Math.PI);
+				}
+			}
+
+			return angle;
+		}
+		/// <summary>
+		/// Creates a number for the facing of the vector
+		/// 261
+		/// 7 5
+		/// 384
+		/// </summary>
+		/// <param name="vector"></param>
+		/// <returns></returns>
+		public static int FacingNumber(Vector2 vector)
+		{
+			double xValue = vector.x;
+			double yValue = vector.y;
+
+			int facing = 0;
+			// positive x and y
+			if ((xValue > 0) && (yValue > 0))
+			{
+				facing = 1;
+			}
+			// x negative y positive
+			else if ((xValue < 0) && (yValue > 0))
+			{
+				facing = 2;
+			}
+			// negative x and y
+			else if ((xValue < 0) && (yValue < 0))
+			{
+				facing = 3;
+			}
+			// positive x, negative y
+			else if ((xValue > 0) && (yValue < 0))
+			{
+				facing = 4;
+			}
+			// positive x axis
+			else if ((xValue > 0) && (yValue == 0))
+			{
+				facing = 5;
+			}
+			// positive y axis
+			else if ((xValue == 0) && (yValue > 0))
+			{
+				facing = 6;
+			}
+			// negative x axis
+			else if ((xValue < 0) && (yValue == 0))
+			{
+				facing = 7;
+			}
+			// negative y axis
+			else if ((xValue == 0) && (yValue < 0))
+			{
+				facing = 8;
+			}
+			// there is no angle or something fell through
+			else
+			{
+				facing = -1;
+			}
+
+			return facing;
+		}
+	}
+}
---
+++ TrianglesInSpace/TrianglesInSpace/Primitives/NUnit/AngleTests.cs
@@ -0,0 +1,124 @@
+using Mogre;
+using NUnit.Framework;
+using Math = System.Math;
+
+namespace TrianglesInSpace.Primitives.NUnit
+{
+	public class AngleTests : TestSpecification
+	{
+		[Test]
+		public static void Reduce_Angle_Test_Greater_Than_Two_Pi()
+		{
+			double angle = Angle.ReduceAngle(3 * Math.PI);
+
+			Assert.AreEqual(Math.PI, angle,0.00001);
+		}
+
+		[Test]
+		public static void Reduce_Angle_Test_Less_Than_Two_Pi()
+		{
+			double angle = Angle.ReduceAngle(-3 * Math.PI);
+
+			Assert.AreEqual(Math.PI, angle, 0.00001);
+		}
+
+		[Test]
+		public static void Create_Angle_Check_Value_Greater_Than_Two_Pi()
+		{
+			Angle angle = new Angle(3 * Math.PI);
+			
+			Assert.AreEqual(Math.PI, angle.Value, 0.00001);
+		}
+
+		[Test]
+		public static void Create_Angle_Check_Value_Less_Than_Two_Pi()
+		{
+			Angle angle = new Angle(-3 * Math.PI);
+
+			Assert.AreEqual(Math.PI, angle.Value, 0.00001);
+		}
+
+		/// <summary>
+		/// Creates a number for the facing of the vector
+		/// 261
+		/// 7 5
+		/// 384
+		/// </summary>
+		/// <returns></returns>
+		
+
+		[Test]
+		public static void Test_First_Quadrant_Facing()
+		{
+			Vector2 vector2 = new Vector2(1,1);
+
+			int facing = Angle.FacingNumber(vector2);
+
+			Assert.AreEqual(1, facing);
+		}
+
+		[Test]
+		public static void Test_Second_Quadrant_Facing()
+		{
+			Vector2 vector2 = new Vector2(-1, 1);
+
+			int facing = Angle.FacingNumber(vector2);
+
+			Assert.AreEqual(2, facing);
+		}
+		[Test]
+		public static void Test_Third_Quadrant_Facing()
+		{
+			Vector2 vector2 = new Vector2(-1, -1);
+
+			int facing = Angle.FacingNumber(vector2);
+
+			Assert.AreEqual(3, facing);
+		}
+		[Test]
+		public static void Test_Fourth_Quadrant_Facing()
+		{
+			Vector2 vector2 = new Vector2(1, -1);
+
+			int facing = Angle.FacingNumber(vector2);
+
+			Assert.AreEqual(4, facing);
+		}
+		[Test]
+		public static void Test_Fifth_Quadrant_Facing()
+		{
+			Vector2 vector2 = new Vector2(1, 0);
+
+			int facing = Angle.FacingNumber(vector2);
+
+			Assert.AreEqual(5, facing);
+		}
+		[Test]
+		public static void Test_Sixth_Quadrant_Facing()
+		{
+			Vector2 vector2 = new Vector2(0, 1);
+
+			int facing = Angle.FacingNumber(vector2);
+
+			Assert.AreEqual(6, facing);
+		}
+		[Test]
+		public static void Test_Seventh_Quadrant_Facing()
+		{
+			Vector2 vector2 = new Vector2(-1, 0);
+
+			int facing = Angle.FacingNumber(vector2);
+
+			Assert.AreEqual(7, facing);
+		}
+		[Test]
+		public static void Test_Eighth_Quadrant_Facing()
+		{
+			Vector2 vector2 = new Vector2(0, -1);
+
+			int facing = Angle.FacingNumber(vector2);
+
+			Assert.AreEqual(8, facing);
+		}
+	}
+}
