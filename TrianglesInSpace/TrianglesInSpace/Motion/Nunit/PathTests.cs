45d46437f953d0c469a6ba88f24d61b7bb1146a9
anthony-martin <martin.anthony.j@gmail.com>
2011-09-13 09:19:00 +0800

    Expand path unit tests to handle more cases

--- TrianglesInSpace/TrianglesInSpace/Motion/Nunit/PathTests.cs
+++ TrianglesInSpace/TrianglesInSpace/Motion/Nunit/PathTests.cs
@@ -10,7 +10,7 @@ namespace TrianglesInSpace.Motion.Nunit
 	public class PathTests : TestSpecification
 	{
 		[Test]
-		public void CreateTurningCircles()
+		public void CreateTurningCirclesBasicPositiveY()
 		{
 			var path = new Path();
 			Vector2 initialVelocity = new Vector2(0,1);
@@ -21,6 +21,88 @@ namespace TrianglesInSpace.Motion.Nunit
 
 			var circleOneResultShouldBe = new Vector2(-1, 0);
 			var circleTwoResultShouldBe = new Vector2(1, 0);
+
+
+			Assert.AreEqual(circleOneResultShouldBe.x, circleOne.x, 0.000000001);
+			Assert.AreEqual(circleOneResultShouldBe.y, circleOne.y, 0.000000001);
+			Assert.AreEqual(circleTwoResultShouldBe.x, circleTwo.x, 0.000000001);
+			Assert.AreEqual(circleTwoResultShouldBe.y, circleTwo.y, 0.000000001);
+		}
+
+
+		[Test]
+		public void CreateTurningCirclesBasicPositiveX()
+		{
+			var path = new Path();
+			Vector2 initialVelocity = new Vector2(1, 0);
+			double acceleration = 1;
+			Vector2 circleOne;
+			Vector2 circleTwo;
+			path.DetermineTurningCircles(initialVelocity, acceleration, out circleOne, out circleTwo);
+
+			var circleOneResultShouldBe = new Vector2(0, 1);
+			var circleTwoResultShouldBe = new Vector2(0, -1);
+
+
+			Assert.AreEqual(circleOneResultShouldBe.x, circleOne.x, 0.000000001);
+			Assert.AreEqual(circleOneResultShouldBe.y, circleOne.y, 0.000000001);
+			Assert.AreEqual(circleTwoResultShouldBe.x, circleTwo.x, 0.000000001);
+			Assert.AreEqual(circleTwoResultShouldBe.y, circleTwo.y, 0.000000001);
+		}
+
+		[Test]
+		public void CreateTurningCirclesBasicNegativeY()
+		{
+			var path = new Path();
+			Vector2 initialVelocity = new Vector2(0, -1);
+			double acceleration = 1;
+			Vector2 circleOne;
+			Vector2 circleTwo;
+			path.DetermineTurningCircles(initialVelocity, acceleration, out circleOne, out circleTwo);
+
+			var circleOneResultShouldBe = new Vector2(1, 0);
+			var circleTwoResultShouldBe = new Vector2(-1, 0);
+
+
+			Assert.AreEqual(circleOneResultShouldBe.x, circleOne.x, 0.000000001);
+			Assert.AreEqual(circleOneResultShouldBe.y, circleOne.y, 0.000000001);
+			Assert.AreEqual(circleTwoResultShouldBe.x, circleTwo.x, 0.000000001);
+			Assert.AreEqual(circleTwoResultShouldBe.y, circleTwo.y, 0.000000001);
+		}
+
+
+		[Test]
+		public void CreateTurningCirclesBasicNegativeX()
+		{
+			var path = new Path();
+			Vector2 initialVelocity = new Vector2(-1, 0);
+			double acceleration = 1;
+			Vector2 circleOne;
+			Vector2 circleTwo;
+			path.DetermineTurningCircles(initialVelocity, acceleration, out circleOne, out circleTwo);
+
+			var circleOneResultShouldBe = new Vector2(0, -1);
+			var circleTwoResultShouldBe = new Vector2(0, 1);
+
+
+			Assert.AreEqual(circleOneResultShouldBe.x, circleOne.x, 0.000000001);
+			Assert.AreEqual(circleOneResultShouldBe.y, circleOne.y, 0.000000001);
+			Assert.AreEqual(circleTwoResultShouldBe.x, circleTwo.x, 0.000000001);
+			Assert.AreEqual(circleTwoResultShouldBe.y, circleTwo.y, 0.000000001);
+		}
+
+		[Test]
+		public void CreateTurningCirclesOnAngle()
+		{
+			var path = new Path();
+			Vector2 initialVelocity = new Vector2(3, 4);
+			double acceleration = 5;
+			Vector2 circleOne;
+			Vector2 circleTwo;
+			path.DetermineTurningCircles(initialVelocity, acceleration, out circleOne, out circleTwo);
+
+			var circleOneResultShouldBe = new Vector2(-4, 3);
+			var circleTwoResultShouldBe = new Vector2(4, -3);
 
 
 			Assert.AreEqual(circleOneResultShouldBe.x, circleOne.x, 0.000000001);
