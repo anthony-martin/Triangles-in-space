ca3106e3c8ba61103359ed8186a6183d14c5ea8e
Anthony Martin <martin.anthony.j@gmail.com>
2013-03-02 10:08:59 +0800

    Add box class for doing box proximity checking

---
+++ TrianglesInSpace/TrianglesInSpace/Primitives/Box.cs
@@ -0,0 +1,51 @@
+﻿namespace TrianglesInSpace.Primitives
+{
+    public struct Box
+    {
+        private readonly Vector m_TopLeftCorner;
+        private readonly Vector m_BottomRightCorner;
+
+        /// <summary>
+        /// A box around 0,0 with X being the horizontal 
+        /// And Y being the vertiacal
+        /// </summary>
+        /// <param name="x">the width of the box</param>
+        /// <param name="y">the height of the box</param>
+        public Box(double x, double y)
+        {
+            const double two = 2.0;
+            m_TopLeftCorner = new Vector(-x / two, y / two);
+            m_BottomRightCorner = new Vector(x / two, -y / two);
+        }
+
+        public Vector TopLeft
+        {
+            get
+            {
+                return m_TopLeftCorner;
+            }
+        }
+
+        public Vector BottomRight
+        {
+            get
+            {
+                return m_BottomRightCorner;
+            }
+        }
+
+        /// <summary>
+        /// Checks to see if positionOne is within the box around positionTwo
+        /// </summary>
+        public bool Contains(Vector positionOne, Vector positionTwo)
+        {
+            var relativePosition = positionOne - positionTwo;
+ 
+            return relativePosition.X >= TopLeft.X
+                   && relativePosition.X <= BottomRight.X
+                   && relativePosition.Y >= BottomRight.Y
+                   && relativePosition.Y <= TopLeft.Y;
+
+        }
+    }
+}
---
+++ TrianglesInSpace/TrianglesInSpace/Primitives/NUnit/BoxTests.cs
@@ -0,0 +1,93 @@
+﻿using NUnit.Framework;
+
+namespace TrianglesInSpace.Primitives.NUnit
+{
+    internal class BoxTests : TestSpecification
+    {
+        [Test]
+        public void TopLeftCorner()
+        {
+            var vector = new Vector(-1, 2.75);
+
+            var box = new Box(2, 5.5);
+
+            Assert.AreEqual(vector, box.TopLeft);
+        }
+
+        [Test]
+        public void BottomLeftCorner()
+        {
+            var vector = new Vector(2, -4);
+
+            var box = new Box(4, 8);
+
+            Assert.AreEqual(vector, box.BottomRight);
+        }
+
+        [Test]
+        public void DoesNotContainIfTooFarLeft()
+        {
+            var posOne = new Vector(-5, 0);
+            var posTwo = new Vector(5, 0);
+
+            var box = new Box(5, 5);
+
+            Assert.False(box.Contains(posOne, posTwo));
+        }
+
+        [Test]
+        public void DoesNotContainIfTooFarRight()
+        {
+            var posOne = new Vector(15, 0);
+            var posTwo = new Vector(5, 0);
+
+            var box = new Box(5, 5);
+
+            Assert.False(box.Contains(posOne, posTwo));
+        }
+
+        [Test]
+        public void ContainsIfWithinXDimension()
+        {
+            var posOne = new Vector(2.5, 0);
+            var posTwo = new Vector(5, 0);
+
+            var box = new Box(5, 5);
+
+            Assert.True(box.Contains(posOne, posTwo));
+        }
+
+        [Test]
+        public void DoesNotContainIfTooLow()
+        {
+            var posOne = new Vector(0, -7);
+            var posTwo = new Vector(0, 3);
+
+            var box = new Box(5, 5);
+
+            Assert.False(box.Contains(posOne, posTwo));
+        }
+
+        [Test]
+        public void DoesNotContainIfTooHigh()
+        {
+            var posOne = new Vector(0, 112);
+            var posTwo = new Vector(0, 3);
+
+            var box = new Box(5, 5);
+
+            Assert.False(box.Contains(posOne, posTwo));
+        }
+
+        [Test]
+        public void ContainsIfWithinVertial()
+        {
+            var posOne = new Vector(0, 5.5);
+            var posTwo = new Vector(0, 3);
+
+            var box = new Box(5, 5);
+
+            Assert.True(box.Contains(posOne, posTwo));
+        }
+    }
+}
